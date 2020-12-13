649e6b28edbf47197e97d9629ab3fcfd541f4912
Akash Kumar Sen <71623442+Akash-Kumar-Sen@users.noreply.github.com>
2020-12-13 22:53:11 +0530

    Updated file.js

--- app/file.js
+++ app/file.js
@@ -1,31 +1,4 @@
-/*let keyToShow = 37;
-let keyToCollapse = 39;
-
-
-function activateDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-function collapseDropdown() {
-    var dropdowns = document.getElementsByClassName("dropdown-menu");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-}
-  
-  document.addEventListener('keydown', function(event) {
-    if(event.keyCode == keyToShow) {
-        activateDropdown();
-    }
-    else if(event.keyCode == keyToCollapse) {
-        collapseDropdown();
-    }
-});*/
-let keyToShow = 37;
+let keyToShow = prompt("Please enter the key code, After pressing that key dropdown will occur");
 
 function activateDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
